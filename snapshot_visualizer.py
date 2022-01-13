3b51dc89cae5f2082bf8873a4c1e142d959e8e57
Emilia Marczyk <emilia.marczyk@rumblefish.pl>
2022-01-13 15:04:58 +0100

    Fix script - messages' structure changed. Display username on the node's label. Add snapshots from app testing

--- snapshot_visualizer.py
+++ snapshot_visualizer.py
@@ -1,9 +1,12 @@
 import json
 import graphviz
+from collections import defaultdict
 
+users_map = dict()
+user_counter = 0
 
 def main():
-    with open('snapshot.json') as snapshot_file:
+    with open('snapshots/snapshot_zdpuB3SW7PZHbXtHAR7GGvD6HQ3RbeYce8LzJvYY2u6BEoxSe.json') as snapshot_file:
         snapshot = snapshot_file.read()
         snapshot = json.loads(snapshot)
     nodes = snapshot['values']
@@ -19,14 +22,27 @@ def main():
     for head in heads:
         parse_graph(graph=graph, node=head, dot=dot, color='red')
 
-    print(dot.source)
+    # print(dot.source)
+    # print(users_map)
     dot.render('snapshot_graph_colors.gv', view=True)
 
+def get_user_name(publicKey):
+    # Generate user name for public key
+    if not users_map.get(publicKey):
+        global user_counter
+        users_map.update({publicKey: f'user{user_counter}'})
+        user_counter += 1
+    return users_map.get(publicKey)
 
 def parse_graph(graph, node, dot, **node_kwargs):
     refs = node['refs']  # ignore refs for now. Maybe draw them with different color?
     current_node_name = node['hash']
-    dot.node(name=current_node_name, label=node['payload']['value'], **node_kwargs)
+    
+    # Map user's public key and display on node's label
+    user_name = get_user_name(node['identity']['publicKey'])
+    label = f"{node['payload']['value']['message']}\n{user_name}"
+
+    dot.node(name=current_node_name, label=label, **node_kwargs)
     nexts = node['next']
     if not nexts:
         return
